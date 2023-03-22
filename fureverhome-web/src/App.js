6af276220816d16409a5e249e8731826eabbd736
Anita Chu <c8anita@gmail.com>
2023-03-22 01:51:16 -0400

    Implemented homepage scroll layout #3

--- fureverhome-web/src/App.js
+++ fureverhome-web/src/App.js
@@ -1,20 +1,38 @@
+import { useRef } from 'react';
 import './App.css';
+import AboutUs from './menuBtn_component/AboutUs';
+import Faq from './menuBtn_component/Faq';
 import catImage from './images/cat.png';
 import dogImage from './images/dog.png';
 
 function App() {
+
+  const aboutUs = useRef(null);
+  const faq = useRef(null);
+
+  const scrollToSec = (e) => {
+    window.scrollTo({
+      top: e.current.offsetTop,
+      behavior: 'smooth'
+    })
+  } 
+
   return (
     <div className="App">
-      <div className='heading_box'>
-        <h1 className='furEverHome_heading'>FurEver Home</h1>
+      <div className='homepage'>
+        <div className='heading_box'>
+          <h1 className='furEverHome_heading'>FurEver Home</h1>
+        </div>
+        <img id='catPic' src={catImage} alt='' />
+        <img id='dogPic' src={dogImage} alt='' />
+        <div className='menu_buttons'>
+          <button className='aboutusBtn' onClick={() => scrollToSec(aboutUs)}>About Us</button>
+          <button className='adoptBtn'>Adopt</button>
+          <button className='faqBtn' onClick={() => scrollToSec(faq)}>FAQ</button>
+        </div>
       </div>
-      <img id='catPic' src={catImage} alt='' />
-      <img id='dogPic' src={dogImage} alt='' />
-      <div className='menu_buttons'>
-        <button className='aboutusBtn'>About Us</button>
-        <button className='adoptBtn'>Adopt</button>
-        <button className='faqBtn'>FAQ</button>
-      </div>
+      <div ref={aboutUs} className='aboutUs_section'><AboutUs /></div>
+      <div ref={faq} className='faq_section'><Faq /></div>
     </div>
   );
 }
