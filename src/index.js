3b3b7fdb04af9b1521274f19b40972ff1aa48ac8
Anita Chu <c8anita@gmail.com>
2023-09-11 11:32:39 -0400

    Set status of private to false in package.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = createRoot(container);
 root.render(
   <React.StrictMode>
       <App />
